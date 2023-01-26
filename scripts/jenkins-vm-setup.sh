e90fca6938c06fc748df4efb806dab2af5a842cb
alvesgf16 <alvesgabriel1994@gmail.com>
2023-01-26 15:54:51 -0300

    ci: add jenkins vm setup script

---
+++ scripts/jenkins-vm-setup.sh
@@ -0,0 +1,10 @@
+curl -fsSL https://get.docker.com | sh
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y openjdk-17-jdk kubectl jenkins
+usermod -aG docker jenkins
+systemctl restart 
+cat /var/lib/jenkins/secrets/initialAdminPassword
